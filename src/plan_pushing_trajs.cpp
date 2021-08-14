f2b0b87f375d349fae0266b447b02f91ac07c1a7
Ali <ali.ghadirzadeh@gmail.com>
2021-08-14 16:45:35 +0200

    work in progress ...

--- src/plan_pushing_trajs.cpp
+++ src/plan_pushing_trajs.cpp
@@ -22,32 +22,20 @@ int main(int argc, char** argv)
   string move_group_name;
   param_node.getParam("/moveit/move_group", move_group_name);
   MoveItPlanner right_arm_planner(move_group_name);
-  // add all fixed obstacles
+
+  // add fixed obstacles
   string filename;
   param_node.getParam("/filenames/fixed_obstacles", filename);
   right_arm_planner.add_fixed_obstacles(filename);
+
   // move both of the arms to the initial position
   param_node.getParam("/initial_joint_position/right_arm", right_joint_position);
   right_arm_planner.plan_and_move_to_joint_goal(right_joint_position);
   sleep(5);
 
-  // add objects
-  int n_objects = 0;
-  param_node.getParam("/collision_objects/number", n_objects);
-  for (int obj = 1; obj <= n_objects; obj++)
-  {
-    std::vector<float> object_dimension;
-    param_node.getParam("/collision_objects/"+std::to_string(obj)+"/dimensions", object_dimension);
-    std::vector<double> object_pose;
-    param_node.getParam("/collision_objects/"+std::to_string(obj)+"/pose", object_pose);
-    right_arm_planner.add_collision_object("object"+std::to_string(obj), object_dimension, create_pose(object_pose));
-  }
-
   std::vector<double> target_area_lowerbound, target_area_upperbound, goal_pose;
   param_node.getParam("/target/area_lower_bound", target_area_lowerbound);
   param_node.getParam("/target/area_upper_bound", target_area_upperbound);
-  std::vector<double> steps;
-  param_node.getParam("/target/steps", steps);
 
   int max_ntraj = 0;
   param_node.getParam("/target/max_number_of_traj", max_ntraj);
@@ -56,7 +44,7 @@ int main(int argc, char** argv)
   char traj_filename[100];
 
   std::cout << std::fixed << std::setprecision(2);
-  std::vector<double> states(3);
+  std::vector<double> states(2);
 
   ros::Publisher r_gripper_pub;
   ros::NodeHandle r_gripper_node;
@@ -66,61 +54,67 @@ int main(int argc, char** argv)
   r_gripper_pub.publish(cmd);
   int traj_counter = 0;
 
-
-  for (double x = target_area_lowerbound[0]; x <= target_area_upperbound[0]; x += steps[0])
+  vector<geometry_msgs::Pose> waypoints;
+  while (traj_counter < max_ntraj)
   {
-    for (double y = target_area_lowerbound[1]; y <= target_area_upperbound[1]; y += steps[1])
-    {      
-      for (double num_samples = 0; num_samples < 10; num_samples++)
+    // sample an initial position
+    double x = target_area_lowerbound[0] + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/(target_area_upperbound[0]-target_area_lowerbound[0])));
+    double y = target_area_lowerbound[1] + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/(target_area_upperbound[1]-target_area_lowerbound[1])));
+
+    // move to the initial position
+    goal_pose[0] = x;
+    goal_pose[1] = y;
+    waypoints.push_back(create_pose(goal_pose));
+    right_arm_planner.plan_cartesian_path(waypoints);
+    right_arm_planner.execute_trajectory();
+    waypoints.clear();
+    sleep(1);
+
+    // sample multiple goal positions
+    for (int num_samples = 0; num_samples < 10; num_samples++)
+    {
+      // measure the pose
+      vector <double> curr_xyz;
+      curr_xyz = right_arm_planner.get_current_xyz();
+      states[0] = curr_xyz[0];
+      states[1] = curr_xyz[1];
+
+      // set the push goal
+      goal_pose = target_area_lowerbound;
+    	double rand_num = static_cast<float>(rand())/(static_cast<float>(RAND_MAX));
+      double dx = (rand_num - 0.5) *0.2;
+    	double dy = sqrt(0.01-dx*dx);
+      if (rand() > (RAND_MAX / 2))
+    	  dy = -dy;
+      goal_pose[0] = x + dx;
+      goal_pose[1] = y + dy;
+      waypoints.push_back(create_pose(goal_pose));
+    	right_arm_planner.plan_cartesian_path(waypoints);
+      waypoints.clear();
+      // saving
+      right_arm_planner.save_trajectory(filename, traj_counter);
+      char state_filename[100];
+      sprintf(state_filename, "%s/states/%04d.txt", filename.c_str(), traj_counter);
+      save_data(state_filename, states);
+      waypoints.clear();
+      if (execute_motion)
       {
-	// set the initial goal 
-	goal_pose = target_area_lowerbound;       
+        right_arm_planner.execute_trajectory();
+        sleep(2);
+        // move back to the initial position
         goal_pose[0] = x;
         goal_pose[1] = y;
-	geometry_msgs::Pose p;
-        vector<geometry_msgs::Pose> waypoints, waypoints2;
-	p = create_pose(goal_pose);
-        waypoints.push_back(p);
-	right_arm_planner.plan_cartesian_path(waypoints);
+        geometry_msgs::Pose p;
+        vector<geometry_msgs::Pose> waypoints;
+        waypoints.push_back(create_pose(goal_pose));
+        right_arm_planner.plan_cartesian_path(waypoints);
         right_arm_planner.execute_trajectory();
+        waypoints.clear();
         sleep(2);
-	// set the push goal 
-	double rand_num = static_cast<float>(rand())/(static_cast<float>(RAND_MAX)); 
-	cout << rand_num << endl;
-        double dx = (rand_num - 0.5) *0.2;	
-	double dy = sqrt(0.01-dx*dx);
-	rand_num = static_cast<float>(rand())/(static_cast<float>(RAND_MAX));
-	cout << rand_num << endl;
-	if (rand_num < 0.5)
-	  dy = -dy;
-	goal_pose[0] += dx;
-	goal_pose[1] += dy;
-	p = create_pose(goal_pose);
-        waypoints2.push_back(p);
-	right_arm_planner.plan_cartesian_path(waypoints2);
-        right_arm_planner.execute_trajectory();
-        sleep(2);
-              
-       	traj_counter++;
-	if (traj_counter >= max_ntraj)
-          break;
       }
-
-      // get current position
-      vector <double> curr_rpy, curr_xyz;
-      curr_rpy = right_arm_planner.get_current_rpy();
-      curr_xyz = right_arm_planner.get_current_xyz();
-      cout << "actual pose:\t" << curr_xyz[0] << "\t" << curr_xyz[1] << "\t"<< curr_xyz[2] << endl;
-      cout << "            \t" << curr_rpy[0] << "\t" << curr_rpy[1] << "\t"<< curr_rpy[2] << endl;
-
-      
-      if (traj_counter >= max_ntraj)
-        break;
-    }
-    if (traj_counter >= max_ntraj)
-    {
-      cout << "max number of trajectories generated! terminating ... " << endl;
-      break;
+      traj_counter++;
+    	if (traj_counter >= max_ntraj)
+              break;
     }
   }
   // termination
